5e946f9e0eafd0219152f8a83d86abe917b86664
Cory <cory.forsyth@gmail.com>
2019-01-24 11:06:12 -0500

    allocating enough memory, but not sure what changed

--- src/lib.rs
+++ src/lib.rs
@@ -41,12 +41,11 @@ impl MyStuff {
     }
   }
 
-  pub fn allocate_data(&mut self, w:u32,h:u32) -> *const u8 {
+  pub fn allocate_data(&mut self, w:u32,h:u32) {
     utils::set_panic_hook();
     self.len = (w*h*4) as usize;
     self.data = Vec::with_capacity(self.len);
     self.data.resize(self.len, 0);
-    self.data.as_ptr()
   }
 
   pub fn set_data(&mut self, data:Vec<u8>, w:u32, h:u32) {
@@ -61,13 +60,6 @@ impl MyStuff {
 
   pub fn get_len(&self) -> usize {
     self.len
-  }
-
-  pub fn munge_data(&mut self) {
-    utils::set_panic_hook();
-    for i in 0..self.data.len() {
-      self.data[i] = self.data[i] / 2;
-    }
   }
 
   pub fn color_data(&mut self, idx:u8) {
